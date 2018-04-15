ba52b0b2c281b636d72aee9937dd6050fe3ca69d
lantzme <eyal.lantz@gmail.com>
2018-04-15 21:27:54 +0300

    Add files via upload

---
+++ ImageService.Logging/LoggingService.cs
@@ -0,0 +1,34 @@
+﻿
+using ImageService.Logging.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+        #region Events
+        public event EventHandler<MessageRecievedEventArgs> MessageRecieved;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// This method logs a message to the logger.
+        /// </summary>
+        /// <param name="message">The message to log in string form.</param>
+        /// <param name="type">The type of message we inform the logger about.</param>
+        public void Log(string message, MessageTypeEnum type)
+        {
+            var msgEventArgs = new MessageRecievedEventArgs
+            {
+                Message = message,
+                Status = type
+            };
+            MessageRecieved?.Invoke(this, msgEventArgs);
+        }
+        #endregion
+    }
+}
